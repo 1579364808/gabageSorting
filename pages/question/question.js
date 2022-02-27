f3ac35a3f3cd63034c8a384b94aac05a871458ed
liulei <1579364808@qq.com>
2022-02-27 10:27:35 +0800

    修复了题目提交界面已知的bug

--- pages/question/question.js
+++ pages/question/question.js
@@ -5,7 +5,7 @@ const questions = db.collection('questions')
 
 Page({
     onSubmit(event) {
-        // console.log(event.detail.value)
+        console.log(event.detail.value)
         let item = event.detail.value
         for (let key in item) {
             // console.log(key)
@@ -23,7 +23,7 @@ Page({
             old = res.data
             console.log(old)
             for (let i = 0; i < old.length; i++) {
-                if (old[i].options[0] === item.A && old[i].options[1] === item.B && old[i].options[2] === item.C && old[i].options[3] === item.D) {
+                if (old[i].options[0] === item.A && old[i].options[1] === item.B && old[i].options[2] === item.C && old[i].options[3] === item.D && old[i].question === item.question) {
                     wx.showModal({
                         showCancel: false,
                         title: '添加失败',
@@ -34,13 +34,9 @@ Page({
 
             }
         })
-
-
-
-
         questions.add({
             data: {
-                questions: item.question.trim(),
+                question: item.question.trim(),
                 options: [item.A.trim(), item.B.trim(), item.C.trim(), item.D.trim()],
                 res: item.res.trim()
             }
@@ -51,13 +47,14 @@ Page({
     },
     onInput(event) {
         console.log(event)
-        if(event.detail.keyCode!=8&&event.detail.keyCode<96||event.detail.keyCode>99){
-            wx.showModal({
-                showCancel: false,
-                title: '警告',
-                content: '请输入0-3的数字',
-            })
+        let keyCode = event.detail.keyCode
+        if (keyCode == 8 || keyCode >= 48 && keyCode <= 51 || keyCode >= 96 && keyCode <= 99) {
             return
         }
+        wx.showModal({
+            showCancel: false,
+            title: '警告',
+            content: '请输入0-3的数字',
+        })
     }
 })
