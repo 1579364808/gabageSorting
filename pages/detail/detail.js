6daa7f67c6ec9e2e7e39a027f21ba23bc34e0a3a
liulei <1579364808@qq.com>
2022-03-03 18:50:56 +0800

    修复一些错误

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,49 +4,54 @@ const reCorderManager = wx.getRecorderManager()
 Page({
 
     data: {
-    gabageName:null,    
-    category:null
+        gabageName: null,
+        category: null
     },
     onLoad: function (event) {
+
         console.log(event.gabageName)
         this.setData({
-            gabageName: event.gabageName
-        }),
-        gabage.where({
-            name: event.gabageName
-        }).get().then(res => {
-            console.log(this.data.gabageName)
-            if(this.data.gabageName){
-                console.log('可以执行搜索')
-                db.collection('gabage')
-                .where({
-                    name: db.RegExp({
-                    regexp:this.data.gabageName,//要搜素的词
-                       options: 'i',//不区分大小写
-                    })
-                  })
-                .get ()
-                .then (res=>{
-                    console.log("成功",res)
-                    this.setData({
-                        list:res.data
-                      })
-                    
-                })
-            
-        }).catch(err=>{
-            wx.showModal({
-                title:'请重新识别或搜索',
-                content: `无法为“${this.data.gabageName}”分类`,
-                showCancel:false, 
-            }).then(res=>{
-                if (res.confirm) {
-                    wx.navigateBack({
-                        delta:2
-                    })
-                  }
+                gabageName: event.gabageName
+            }),
+            gabage.where({
+                name: event.gabageName
             })
-        })
-    }
+            .get()
+            .then(res => {
+                console.log(this.data.gabageName)
+                if (this.data.gabageName) {
+                    console.log('可以执行搜索')
+                    db.collection('gabage')
+                        .where({
+                            name: db.RegExp({
+                                regexp: this.data.gabageName, //要搜素的词
+                                options: 'i', //不区分大小写
+                            })
+                        })
+                        .get()
+                        .then(res => {
+                            console.log("成功", res)
+                            this.setData({
+                                list: res.data
+                            })
+                        })
+                }
+            })
+            .catch(err => {
+                wx.showModal({
+                        title: '请重新识别或搜索',
+                        content: `无法为“${this.data.gabageName}”分类`,
+                        showCancel: false,
+                    })
+                    .then(res => {
+                        if (res.confirm) {
+                            wx.navigateBack({
+                                delta: 2
+                            })
+                        }
+                    })
 
+            })
+
+    }
 })
