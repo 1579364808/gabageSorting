6aa7fb17faf9fb1304758d35266c87153eecc717
liulei <1579364808@qq.com>
2022-03-26 13:47:44 +0800

    实现了基础的语音识别

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,33 +1,47 @@
 // index.js
-
+const recorderManager = wx.getRecorderManager() //这是录音功能的实例，必须的
+const fs = wx.getFileSystemManager() //文件管理系统
 Page({
     data: {
         category: null, //垃圾种类
         showRadio: false,
         gabages: [],
-        selectedItem:null,
-        buttonDisable:false,
+        selectedItem: null,
+        buttonDisable: false,
     },
     onLoad(event) {
-         // ---------------------------------------------------------
+        // ---------------------------------------------------------
         this.setData({
             showDialog: false,
-            selectedItem:null,
+            selectedItem: null,
         })
         //加载时调用云函数获取token加入缓存
         wx.cloud.callFunction({
-            name: "baiduAccessToken",
+            name: "imgToken",
         }).then(res => {
+            console.log(res)
             let token = res.result.data.access_token
             //写入缓存
             wx.setStorage({
-                key: 'baidutoken',
+                key: 'imgToken',
                 data: {
                     token: token
                 }
             })
         })
-   
+        wx.cloud.callFunction({
+            name: "audioToken",
+        }).then(res => {
+            console.log(res)
+            let token = res.result.data.access_token
+            //写入缓存
+            wx.setStorage({
+                key: 'audioToken',
+                data: {
+                    token: token
+                }
+            })
+        })
 
 
     },
@@ -36,23 +50,91 @@ Page({
             url: `../detail/detail?gabageName=${event.detail}`,
         })
     },
-    onClick(event) {
-        let fs = wx.getFileSystemManager() //获取文件管理系统
+    //语音识别    
+    beginRecord(event) {
+        console.log(event);
+        //录音的参数
+        const options = {
+            duration: 3000, //录音时间，默认是3s，提前松手会触发button的bindtouchend事件，执行停止函数并上传录音文件。
+            sampleRate: 16000,
+            numberOfChannels: 1,
+            encodeBitRate: 48000,
+            format: 'pcm'
+        }
+        recorderManager.start(options);
+
+    },
+    endRecord(event) {
+        console.log(event);
+        recorderManager.stop();
+        //监听录音结束
+        recorderManager.onStop(res => {
+            console.log(res);
+            let path = res.tempFilePath;
+            console.log(path)
+          fs.getFileInfo({
+                filePath:path,
+                success:res=>{
+                    let len = res.size
+                    let audio = fs.readFileSync(path, 'base64')
+                    let token = wx.getStorageSync('audioToken').token
+                    this.audio_rq(audio,token,len)
+                }
+            })
+
+            显示加载
+            wx.showLoading({
+                title: '正在识别中',
+            })
+        })
+
+    },
+    // //语音识别请求
+    audio_rq(audio,token,fileSize) {
+        let that = this
+        wx.request({
+            //进行post请求
+            method: 'post',
+            url: 'https://vop.baidu.com/pro_api',
+            
+            //请求的参数
+            data: {
+                format: 'pcm', //语音格式
+                rate: 16000, //采样率
+                channel: 1, //声道数
+                cuid: "MelodyOfTears", //用户唯一标识  
+                token:token,
+                speech:audio,
+                len:fileSize,
+                dev_pid:80001
+            },
+            header: {
+                'content-type': 'application/json'
+            },
+            success(res) {
+                console.log(res);
+            }
+        })
+
+    },
+    //图片识别    
+    img_recog_onClick(event) {
+
         let path = event.detail.file.url
         let image = fs.readFileSync(path, 'base64') //将图片转换成base64
-
+     
         //请求
-        let token = wx.getStorageSync('baidutoken').token
-
+        let token = wx.getStorageSync('imgToken').token
+      
         console.log(token)
-        this.rq(image, token)
+        this.img_rq(image, token)
         //显示加载
         wx.showLoading({
             title: '正在识别中',
         })
     },
-    //识别请求
-    rq(image, token) {
+    //图片识别请求
+    img_rq(image, token) {
         let that = this
         wx.request({
             //进行post请求
@@ -69,36 +151,36 @@ Page({
             },
             success(res) {
                 wx.hideLoading()
-                let  items  = [];
+                let items = [];
                 for (let i = 0; i < res.data.result.length; i++) {
                     let temp = {
-                        name:res.data.result[i].keyword,
-                        id:i
+                        name: res.data.result[i].keyword,
+                        id: i
                     }
                     //对象数组
                     items.push(temp)
                 }
                 console.log(items)
                 that.setData({
-                    showRadio:true,
+                    showRadio: true,
                     gabages: items //获取物品集
-                })             
+                })
             }
         })
     },
-    ridio_event(event){
+    ridio_event(event) {
         this.setData({
-            selectedItem:event.detail.value
-        }) 
+            selectedItem: event.detail.value
+        })
     },
-    confirm(){
-        if(this.data.selectedItem!=null){
+    pick_confirm() {
+        if (this.data.selectedItem != null) {
             this.setData({
-                buttonDisable:true,   
-            }) 
+                buttonDisable: true,
+            })
             wx.redirectTo({
                 url: `../detail/detail?gabageName=${this.data.selectedItem}`,
             })
-        }   
+        }
     }
 })
