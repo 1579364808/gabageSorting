6d49c386fec128de2b38f250e7083daf9195b982
lingjing-wq <3533895097@qq.com>
2022-03-29 20:41:09 +0800

    未完待续——指南部分（列表显示存在问题）

--- pages/guide/guide.js
+++ pages/guide/guide.js
@@ -1,4 +1,6 @@
 // pages/guide/guide.js
+const db=wx.cloud.database()
+let page=0
 Page({
     data: {
       flag: 0,
@@ -17,6 +19,119 @@ Page({
       page.setData({
         flag: id
       });
-    }
+      /////
+      if(e.target.id==0)
+      {
+        db.collection("gabage")
+        .where({
+          category:"干垃圾"
+        })
+        .skip(20)
+        .get()
+        .then(res=>{
+            console.log("请求成功",res);
+            this.setData({
+              list0:res.data
+            })
+        })
+        .catch(err=>{
+            console.log("请求失败",err);
+        })
+      }
+      if(e.target.id==1)
+      {
+       
+        db.collection("gabage")
+        .where({
+          category:"湿垃圾"
+        })
+        .skip(20)
+        .get()
+        .then(res=>{
+            console.log("请求成功",res);
+            this.setData({
+              list1:res.data
+            })
+        })
+        .catch(err=>{
+            console.log("请求失败",err);
+        })
+      }
+      if(e.target.id==2)
+      {
+        db.collection("gabage")
+        .where({
+          category:"可回收垃圾"
+        })
+        .skip(20)
+        .get()
+        .then(res=>{
+            console.log("请求成功",res);
+            this.setData({
+              list2:res.data
+            })
+        })
+        .catch(err=>{
+            console.log("请求失败",err);
+        })
+      }
+      if(e.target.id==3)
+      {
+        db.collection("gabage")
+        .where({
+          category:"有害垃圾"
+        })
+        .skip(20)
+        .get()
+        .then(res=>{
+            console.log("请求成功",res);
+            this.setData({
+              list3:res.data
+            })
+        })
+        .catch(err=>{
+            console.log("请求失败",err);
+        })
+      }
+      
+    },
+    catchTouchMove: function (res) {
+      return false
+    },
+    // getList(){
+    //   console.log("长度",this.data.list0.length);
+    //   let len=this.data.list0.length
+    //     db.collection("gabage")
+    //     .where({
+    //       category:"干垃圾"
+    //     })
+    //     // .limit()
+    //     .skip(len)
+    //     .get()
+    //     .then(res=>{
+    //         console.log("请求成功",res);
+    //         if(res.data.length>20){
+    //               wx.showToast({
+    //                 title: '没有更多数据啦',
+    //               })
+    //         }
+    //         this.setData({
+    //           list0:this.data.list0.concat(res.data)
+    //         })
+    //     })
+    //     .catch(err=>{
+    //         console.log("请求失败",err);
+    //     })
+    // },
+    // onReachBottom:function(){
+    //   console.log("onReachBottom");
+    //   if(page<=1)
+    //   {
+    //       page++ 
+    //       this.getList()
+    //   }
+     
+    // }
+
   })
   
